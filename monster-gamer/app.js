ac0002e5d4beb85ceb2c20c12265744256c95c04
Your Name <you@example.com>
2023-08-07 23:12:57 +0100

    implementing the attack functionality

---
+++ monster-gamer/app.js
@@ -0,0 +1,27 @@
+function getRandomValue(max, min) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100
+    };
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(12, 5);
+      //   this.monsterHealth = this.monsterHealth - attaclValue;
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      attackValue = getRandomValue(15, 8);
+      this.playerHealth -= attackValue;
+      //   this.attackMonster();
+    }
+  }
+});
+
+app.mount("#game");
