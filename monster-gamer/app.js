74b863ceb314c8e6ef8343bbf3cf567276372480
Your Name <you@example.com>
2023-08-08 01:19:26 +0100

    final implementaion

--- monster-gamer/app.js
+++ monster-gamer/app.js
@@ -14,9 +14,15 @@ const app = Vue.createApp({
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHealth + "%" };
     },
     mayUseSpecialAttack() {
@@ -44,6 +50,12 @@ const app = Vue.createApp({
     }
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
@@ -70,6 +82,10 @@ const app = Vue.createApp({
         this.playerHealth += healValue;
       }
       this.attackPlayer();
+    },
+
+    surrender() {
+      this.winner = "monster";
     }
   }
 });
